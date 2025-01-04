eda215163b52d1213e295277793f4b7b9343c9cb
Sateerrahimi <haroonsateer@gmail.com>
2025-01-04 22:27:39 +0100

    Remove H2 database configuration and unused driver class
    Deleted test database properties and the unused `Driver` class to clean up redundant configurations and code. Simplified the test setup by removing unnecessary profile-specific setups for better maintainability.

--- src/test/java/org/example/mobile/MobileApplicationTests.java
+++ src/test/java/org/example/mobile/MobileApplicationTests.java
@@ -1,21 +1,12 @@
 package org.example.mobile;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class MobileApplicationTests {
-
-    @Autowired
-    private Environment environment;
-
     @Test
     public void contextLoads() {
-        System.out.println("Active Profiles: " + String.join(", ", environment.getActiveProfiles()));
     }
 }
