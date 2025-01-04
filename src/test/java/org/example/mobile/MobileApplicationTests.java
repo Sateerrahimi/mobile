59aeba98081fd811807d64a4334bbaf5b6bade04
Sateerrahimi <haroonsateer@gmail.com>
2025-01-04 21:59:10 +0100

    Set active profile to 'test' in MobileApplicationTests
    Added the `@ActiveProfiles` annotation to ensure tests run with the 'test' profile active. This aids in isolating test configurations and improves testing consistency. Additionally, included a debug statement to print active profiles.

--- src/test/java/org/example/mobile/MobileApplicationTests.java
+++ src/test/java/org/example/mobile/MobileApplicationTests.java
@@ -2,13 +2,16 @@ package org.example.mobile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MobileApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Active Profiles:");
     }
 
 }
